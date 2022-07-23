f0ab0d45aa5a3b17160e52e41dddc1874d97439b
Denys Telychko <denys_telychko@icloud.com>
2022-07-23 12:19:38 +0300

    first commit

---
+++  prompt/lesson_6/task4.js
@@ -0,0 +1,20 @@
+let i = 0;
+while (i < 3) {
+ i += 1;
+}
+ {
+    let newStudent = prompt('Введите имя нового студента!'); newStudent = newStudent.trim();
+    if (newStudent) {
+    alert(`Добро пожаловать, ${newStudent}!`) }
+    }
+    
+    let j = 0;
+    do {
+        let newStudent = prompt('Введите имя нового студента!');
+        if (newStudent) {
+ newStudent = newStudent.trim();
+        alert(`Добро пожаловать, ${newStudent}!`);
+    } 
+     j + 1;
+ } while (j < 3);
+       
